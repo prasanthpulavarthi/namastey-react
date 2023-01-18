9c745b7445ffd54dff3f41708bc7f965e8c8f855
prasanth pulavarthi <prasanthpulavarthi19@gmail.com>
2023-01-18 17:55:02 +0530

    igniting our app completed

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react"; 
+import ReactDOM from "react-dom/client"
+const heading1 = React.createElement("h1", {}, "heading1 is from parcel");
+const heading2 = React.createElement("h2", {}, "heading2");
+const container = React.createElement("div", {}, [heading1,heading2]);
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+      root.render(container);
